fd2b7c20e9807de0e720191aef396b7354481be7
Vanessa <lly219@gmail.com>
2015-06-20 14:42:46 +0800

    modify tools :+1:

---
+++ toolers/Gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require ('gulp'),
+minifycss = require('gulp-minify-css'),
+rename = require('gulp-rename'),
+uglify = require ('gulp-uglify'),
+clean = require('gulp-clean');
+
+gulp.task ('clean', function () {
+	gulp.src('../*/css/*.min.css').pipe(clean({force: true}));
+
+	gulp.src('../*/js/*.min.js').pipe(clean({force: true}));
+});
+
+gulp.task ('build', function () {
+    gulp.src ('../*/css/*.css')
+    .pipe(rename({ suffix: '.min' }))
+	.pipe(minifycss())
+	.pipe(gulp.dest ('../'));
+
+    gulp.src ('../*/js/*.js')
+    .pipe(rename({ suffix: '.min' }))
+	.pipe(uglify())
+	.pipe(gulp.dest ('../'));
+});
