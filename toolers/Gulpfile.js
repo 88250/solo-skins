247746160e4e4b7cb03b4339cb26133ca229de31
Van <lly219@gmail.com>
2016-11-10 11:28:41 +0800

    support license for compress css

--- toolers/Gulpfile.js
+++ toolers/Gulpfile.js
@@ -2,21 +2,35 @@ var gulp = require ('gulp'),
 minifycss = require('gulp-minify-css'),
 rename = require('gulp-rename'),
 uglify = require ('gulp-uglify'),
-clean = require('gulp-clean');
+clean = require('gulp-clean'),
+license = require('gulp-header-license'),
+fs = require('fs');
 
-gulp.task ('clean', function () {
-	gulp.src('../*/css/*.min.css').pipe(clean({force: true}));
+gulp.task ('clean', function (cb) {
+	var stream = gulp.src('../*/css/*.min.css').pipe(clean({force: true}));
 	gulp.src('../*/js/*.min.js').pipe(clean({force: true}));
+
+	return stream;
 });
 
-gulp.task ('build', function () {
-    gulp.src ('../*/css/*.css')
+gulp.task ('build', ['clean'], function (cb) {
+    var stream = gulp.src ('../*/css/*.css')
     .pipe(rename({ suffix: '.min' }))
-	.pipe(minifycss({keepSpecialComments: '1'}))
+	.pipe(minifycss())
 	.pipe(gulp.dest ('../'));
 
     gulp.src ('../*/js/*.js')
     .pipe(rename({ suffix: '.min' }))
 	.pipe(uglify({preserveComments: 'license'}))
 	.pipe(gulp.dest ('../'));
+
+	return stream;
 });
+
+gulp.task ('license', ['clean', 'build'], function () {
+	gulp.src('../*/css/*.min.css')
+            .pipe(license(fs.readFileSync('header.txt', 'utf8'), {year: (new Date()).getFullYear()}, 1))
+            .pipe(gulp.dest('../'));
+});
+
+gulp.task('default', ['clean', 'build', 'license']);
